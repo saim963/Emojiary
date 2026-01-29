d18d71a24ffba9e6839a32ffec31b2d9141eed97
Saim Shakeel <aims3328@gmail.com>
2026-01-29 15:59:25 +0530

    Bump version to 1.0.3 (versionCode 4) for closed testing

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "in.cintech.daymoji"
         minSdk = 26
         targetSdk = 35
-        versionCode = 3
-        versionName = "1.0.2"
+        versionCode = 4
+        versionName = "1.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
